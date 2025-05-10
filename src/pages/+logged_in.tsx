362d39ff5e15edac27c78f25e47d62f1b42dfe35
SharpMars <70289680+SharpMars@users.noreply.github.com>
2025-05-10 20:10:56 +0200

    Pagination Buttons style pass

--- src/pages/+logged_in.tsx
+++ src/pages/+logged_in.tsx
@@ -179,11 +179,13 @@ export default function LoggedIn() {
                 <button onClick={() => setSearchVal("")}>⨉</button>
               </div>
             </div>
-            <PaginationButtons
-              currentIndex={currentIndex}
-              setCurrentIndex={setCurrentIndex}
-              pageCount={Math.ceil(filteredLikes()?.length / 50)}
-            ></PaginationButtons>
+            <div class="m-y-2">
+              <PaginationButtons
+                currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+                pageCount={Math.ceil(filteredLikes()?.length / 50)}
+              ></PaginationButtons>
+            </div>
           </div>
           <div class="flex justify-center">
             <ul class="flex flex-col gap-2">
@@ -204,7 +206,7 @@ export default function LoggedIn() {
               />
             </ul>
           </div>
-          <div class="m-t-1">
+          <div class="m-t-2">
             <PaginationButtons
               currentIndex={currentIndex}
               setCurrentIndex={setCurrentIndex}
@@ -248,7 +250,7 @@ function PaginationButtons(props: {
   pageCount: number;
 }) {
   return (
-    <div class="flex justify-center flex-row">
+    <div class="flex justify-center flex-row text-white">
       <button
         onclick={() =>
           props.setCurrentIndex(
@@ -257,55 +259,62 @@ function PaginationButtons(props: {
               : props.currentIndex() - 1
           )
         }
+        class="rounded-l-lg dark:bg-slate-700 light:bg-slate-400 p-2 text-5 dark:hover:bg-slate-800 light:hover:bg-slate-500 dark:active:bg-slate-900 light:active:bg-slate-600 transition-all transition-100 transition-ease-linear dark:b-slate-700 light:b-slate-400 b-l-1 b-t-1 b-b-1 b-solid"
       >
-        🐀
+        <div class="i-mingcute-left-fill"></div>
       </button>
-      <input
-        type="number"
-        min={1}
-        max={props.pageCount}
-        value={props.currentIndex() + 1}
-        onkeypress={(e) => {
-          if (e.key != "Enter") return;
+      <div class="flex items-center dark:bg-slate-700 light:bg-slate-400 b-t-1 b-b-1 dark:b-slate-700 light:b-slate-400 b-solid p-x-1">
+        <input
+          type="number"
+          min={1}
+          max={props.pageCount}
+          value={props.currentIndex() + 1}
+          onkeypress={(e) => {
+            if (e.key != "Enter") return;
 
-          const val = e.currentTarget.value;
-          try {
-            const newIndex = parseInt(val);
+            const val = e.currentTarget.value;
+            try {
+              const newIndex = parseInt(val);
 
-            if (newIndex > props.pageCount) throw new Error();
+              if (newIndex > props.pageCount) throw new Error();
 
-            if (newIndex < 1) throw new Error();
+              if (newIndex < 1) throw new Error();
 
-            props.setCurrentIndex(newIndex - 1);
-            e.currentTarget.blur();
-          } catch (error) {
-            e.currentTarget.value = "" + props.currentIndex() + 1;
-            e.currentTarget.blur();
-          }
-        }}
-        onblur={(e) => {
-          const val = e.target.value;
-          try {
-            const newIndex = parseInt(val);
+              props.setCurrentIndex(newIndex - 1);
+              e.currentTarget.blur();
+            } catch (error) {
+              e.currentTarget.value = "" + props.currentIndex() + 1;
+              e.currentTarget.blur();
+            }
+          }}
+          onblur={(e) => {
+            const val = e.target.value;
+            try {
+              const newIndex = parseInt(val);
 
-            if (newIndex > props.pageCount) throw new Error();
+              if (newIndex > props.pageCount) throw new Error();
 
-            if (newIndex < 1) throw new Error();
+              if (newIndex < 1) throw new Error();
 
-            props.setCurrentIndex(newIndex - 1);
-          } catch (error) {
-            e.target.value = "" + props.currentIndex() + 1;
-          }
-        }}
-        class="w-min field-sizing-content text-center m-0 moz-appearance-textfield [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none light:text-black dark:text-white"
-      ></input>
-      <p class="light:text-black dark:text-white">/{props.pageCount}</p>
+              props.setCurrentIndex(newIndex - 1);
+            } catch (error) {
+              e.target.value = "" + props.currentIndex() + 1;
+            }
+          }}
+          class="w-min field-sizing-content text-center m-0 moz-appearance-textfield [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none p-x-2"
+        ></input>
+        <span class="dark:text-slate-400 light:text-white scale-y-150 translate-y--10% select-none">
+          /
+        </span>
+        <p class="p-x-2">{props.pageCount}</p>
+      </div>
       <button
         onclick={() =>
           props.setCurrentIndex((props.currentIndex() + 1) % props.pageCount)
         }
+        class="rounded-r-lg dark:bg-slate-700 light:bg-slate-400 p-2 text-5 dark:hover:bg-slate-800 light:hover:bg-slate-500 dark:active:bg-slate-900 light:active:bg-slate-600 transition-all transition-100 transition-ease-linear dark:b-slate-700 light:b-slate-400 b-r-1 b-t-1 b-b-1 b-solid"
       >
-        🐀
+        <div class="i-mingcute-right-fill"></div>
       </button>
     </div>
   );
