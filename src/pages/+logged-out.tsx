ab2527d7b1250edf0914d6bda1eafc1dc7c46585
SharpMars <70289680+SharpMars@users.noreply.github.com>
2025-05-10 20:47:08 +0200

    Update logged out page text

--- src/pages/+logged-out.tsx
+++ src/pages/+logged-out.tsx
@@ -7,9 +7,10 @@ export default function LoggedOut() {
           <span class="text-yellow-500">Catalog</span>
         </h1>
         <p class="text-[clamp(8px,7vw,2rem)] text-center light:text-black dark:text-white">
-          Search through your
-          <br /> likes and bookmarks <b>easily</b>
+          Search through your Bluesky
+          <br /> likes and bookmarks* <b>easily</b>
         </p>
+        <p class="text-gray">*bookmarks soon</p>
         <button
           onClick={() => document.getElementById("login").click()}
           class="light:bg-sky-500 light:hover:bg-sky-400 dark:bg-sky-600 dark:hover:bg-sky-500 text-white p-2 rounded m-t-4 w-48 transition-ease-linear transition-all transition-100"
