6b2889d4e310ff401d7e8d8dc42918e836585e39
SharpMars <70289680+SharpMars@users.noreply.github.com>
2025-05-28 01:30:46 +0200

    Loading screen for logged in state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Show } from "solid-js";
+import { Show, Suspense } from "solid-js";
 import { loginState } from "../app";
 import LoggedOut from "./+logged-out";
 import LoggedIn from "./+logged_in";
+import { LoadingIndicator } from "../components/LoadingIndicator";
 
 export default function Home() {
   return (
@@ -10,7 +11,15 @@ export default function Home() {
         <LoggedOut></LoggedOut>
       </Show>
       <Show when={loginState()}>
-        <LoggedIn></LoggedIn>
+        <Suspense
+          fallback={
+            <div class="h-screen-md max-h-[calc(100vh-4rem)] flex flex-col justify-center items-center text-32">
+              <LoadingIndicator></LoadingIndicator>
+            </div>
+          }
+        >
+          <LoggedIn></LoggedIn>
+        </Suspense>
       </Show>
     </>
   );
