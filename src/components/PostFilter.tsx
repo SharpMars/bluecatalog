174256154738855a4774e5dc6acf341f1f763485
SharpMars <70289680+SharpMars@users.noreply.github.com>
2025-09-28 15:05:48 +0200

    Search for author filter

--- src/components/PostFilter.tsx
+++ src/components/PostFilter.tsx
@@ -1,11 +1,12 @@
 import Popover from "@corvu/popover";
-import { Accessor, createSignal, For, Setter, Show, Suspense } from "solid-js";
+import { Accessor, createEffect, createSignal, For, Setter, Show, Suspense } from "solid-js";
 import { SetStoreFunction, Store } from "solid-js/store";
 import { AppBskyActorDefs } from "@atcute/bluesky";
 import { createVirtualizer } from "@tanstack/solid-virtual";
 import { useInfiniteQuery } from "@tanstack/solid-query";
 import { xrpc } from "../app";
 import { FetchData } from "../fetching/fetch-data";
+import MiniSearch, { SearchResult } from "minisearch";
 
 function AuthorsPopup(props: {
   authors: FetchData["authors"];
@@ -13,6 +14,46 @@ function AuthorsPopup(props: {
   setSelectedAuthors: Setter<string[]>;
 }) {
   let authorListRef: HTMLDivElement;
+  let searchIndexSelectorRef: HTMLInputElement;
+
+  const searcher = new MiniSearch({
+    fields: ["handle"],
+    extractField: (document, fieldName) => {
+      const author = document as { id: number; handle: string };
+      if (fieldName == "id") {
+        return author.id.toString();
+      }
+
+      if (fieldName == "handle") {
+        return author.handle;
+      }
+    },
+  });
+
+  const [searchRes, setSearchRes] = createSignal<SearchResult[]>([]);
+  const [searchIndex, setSearchIndex] = createSignal<number>(0);
+
+  createEffect(() => {
+    searcher.removeAll();
+    searcher.addAll(props.authors.map((value, i) => ({ id: i, handle: value.handle })));
+  });
+
+  createEffect(() => {
+    if (searchRes().length > 0) {
+      searchIndexSelectorRef.valueAsNumber = 1;
+      setSearchIndex(1);
+    } else {
+      searchIndexSelectorRef.valueAsNumber = 0;
+      setSearchIndex(0);
+    }
+  });
+
+  createEffect(() => {
+    if (searchRes().length > 0 && searchIndex() > 0) {
+      const selectedRes = searchRes()[searchIndex() - 1];
+      if (selectedRes) rowVirtualizer.scrollToIndex(parseInt(selectedRes.id), { align: "start", behavior: "auto" });
+    }
+  });
 
   const [reqPage, setReqPage] = createSignal(0);
 
@@ -50,62 +91,146 @@ function AuthorsPopup(props: {
   }
 
   return (
-    <div ref={authorListRef} class="h-full overflow-y-auto relative">
-      <div style={{ width: "100%", height: `${rowVirtualizer.getTotalSize()}px` }}>
-        <For each={rowVirtualizer.getVirtualItems()}>
-          {(item) => {
-            const did = props.authors[item.index].did;
-            const pageId = Math.floor(item.index / 25);
+    <>
+      <div class="flex gap-2 h-7">
+        <input
+          class="flex-grow-1 dark:bg-slate-900 rounded-md p-l-1 min-w-0"
+          type="text"
+          placeholder="Search..."
+          oninput={(ev) => {
+            const value = ev.currentTarget.value;
+            setSearchRes(searcher.search(value, { fuzzy: 0.3, prefix: true }));
+          }}
+        />
+        <div class="flex gap-1 items-center flex-nowrap text-nowrap">
+          <input
+            class="min-w-0 dark:bg-slate-900 rounded-md p-l-1 overflow-hidden h-full [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+            value={searchIndex()}
+            ref={searchIndexSelectorRef}
+            type="number"
+            min={searchRes().length > 0 ? 1 : 0}
+            max={searchRes().length}
+            onchange={(ev) => {
+              console.log("onchange");
 
-            requestPage(pageId);
+              if (isNaN(ev.currentTarget.valueAsNumber))
+                ev.currentTarget.valueAsNumber = searchRes().length > 0 ? 1 : 0;
+              setSearchIndex(ev.currentTarget.valueAsNumber);
+            }}
+            oninput={(ev) => {
+              let val = ev.currentTarget.valueAsNumber;
 
-            const avatar = () => {
-              if (avatarQuery.data) {
-                const pageIndex = avatarQuery.data.pageParams.findIndex((val) => val == pageId);
-                if (pageIndex == -1) return "./fallback.svg";
+              if (ev.currentTarget.value == "") return;
 
-                const page = avatarQuery.data.pages[pageIndex];
-                if (page && page.has(did)) return page.get(did);
+              if (isNaN(val)) {
+                ev.currentTarget.valueAsNumber = searchRes().length > 0 ? 1 : 0;
+                //setSearchIndex(searchRes().length > 0 ? 1 : 0);
               }
 
-              return "./fallback.svg";
-            };
+              if (searchRes().length > 0) {
+                if (val > searchRes().length) val = searchRes().length;
 
-            return (
-              <button
-                class="max-h-9 h-9 h-full flex items-center gap-2 w-full p-x-1 rounded p-y-2 hover:bg-black/50 [&:not(.toggled)]:hover:bg-black/20 [&.toggled]:bg-black/40 transition-all transition-100 transition-ease-linear"
-                classList={{
-                  toggled: props.selectedAuthors().find((val) => val == did) != undefined,
-                }}
-                style={{
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  width: "100%",
-                  height: `${item.size}px`,
-                  transform: `translateY(${item.start}px)`,
-                }}
-                onclick={(ev) => {
-                  ev.currentTarget.classList.toggle("toggled");
-                  if (props.selectedAuthors().find((val1) => val1 == did) == undefined) {
-                    props.setSelectedAuthors([did, ...props.selectedAuthors()]);
-                  } else {
-                    props.setSelectedAuthors(props.selectedAuthors().filter((val1) => val1 != did));
-                  }
-                }}
-              >
-                <Suspense
-                  fallback={<img class="aspect-ratio-square max-h-7 rounded" src={"./fallback.svg"} loading="lazy" />}
-                >
-                  <img class="aspect-ratio-square max-h-7 rounded" src={avatar()} loading="lazy" />
-                </Suspense>
-                <span>{props.authors[item.index].handle}</span>
-              </button>
-            );
-          }}
-        </For>
+                if (val < 1) val = 1;
+              } else val = 0;
+
+              ev.currentTarget.valueAsNumber = val;
+              setSearchIndex(val);
+            }}
+          />
+          <span class="select-none">/ {searchRes().length} </span>
+        </div>
+        <div class="flex gap-2 text-5">
+          <button
+            class="w-fit p-1 rounded-xl hover:bg-white/25 active:bg-white/50 aspect-ratio-square flex justify-center items-center transition-all transition-100 transition-ease-in-out"
+            onclick={() => {
+              if (searchRes().length == 0) return;
+
+              setSearchIndex((searchIndex() + 1) % (searchRes().length + 1) == 0 ? 1 : searchIndex() + 1);
+            }}
+          >
+            <div class="i-mingcute-up-line" />
+          </button>
+          <button
+            class="w-fit p-1 rounded-xl hover:bg-white/25 active:bg-white/50 aspect-ratio-square flex justify-center items-center transition-all transition-100 transition-ease-in-out"
+            onclick={() => {
+              if (searchRes().length == 0) return;
+
+              setSearchIndex(searchIndex() - 1 < 1 ? searchRes().length : searchIndex() - 1);
+            }}
+          >
+            <div class="i-mingcute-down-line" />
+          </button>
+        </div>
       </div>
-    </div>
+      <div ref={authorListRef} class="h-full overflow-y-auto relative">
+        <div style={{ width: "100%", height: `${rowVirtualizer.getTotalSize()}px` }}>
+          <For each={rowVirtualizer.getVirtualItems()}>
+            {(item) => {
+              const did = props.authors[item.index].did;
+              const pageId = Math.floor(item.index / 25);
+
+              requestPage(pageId);
+
+              const avatar = () => {
+                if (avatarQuery.data) {
+                  const pageIndex = avatarQuery.data.pageParams.findIndex((val) => val == pageId);
+                  if (pageIndex == -1) return "./fallback.svg";
+
+                  const page = avatarQuery.data.pages[pageIndex];
+                  if (page && page.has(did)) return page.get(did);
+                }
+
+                return "./fallback.svg";
+              };
+
+              const isHighlighted = () => {
+                return !!searchRes().find((val) => parseInt(val.id) == item.index);
+              };
+
+              return (
+                <button
+                  class="max-h-9 h-9 h-full flex items-center gap-2 w-full p-x-1 rounded p-y-2 hover:bg-black/50 [&:not(.toggled)]:hover:bg-black/20 [&.toggled]:bg-black/40
+                  transition-all transition-100 transition-ease-linear"
+                  classList={{
+                    toggled: props.selectedAuthors().find((val) => val == did) != undefined,
+                  }}
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    width: "100%",
+                    height: `${item.size}px`,
+                    transform: `translateY(${item.start}px)`,
+                  }}
+                  onclick={(ev) => {
+                    ev.currentTarget.classList.toggle("toggled");
+                    if (props.selectedAuthors().find((val1) => val1 == did) == undefined) {
+                      props.setSelectedAuthors([did, ...props.selectedAuthors()]);
+                    } else {
+                      props.setSelectedAuthors(props.selectedAuthors().filter((val1) => val1 != did));
+                    }
+                  }}
+                >
+                  <Suspense
+                    fallback={<img class="aspect-ratio-square max-h-7 rounded" src={"./fallback.svg"} loading="lazy" />}
+                  >
+                    <img class="aspect-ratio-square max-h-7 rounded" src={avatar()} loading="lazy" />
+                  </Suspense>
+                  <span
+                    class="[&.highlight]:bg-amber [&.highlight]:text-black"
+                    classList={{
+                      highlight: isHighlighted(),
+                    }}
+                  >
+                    {props.authors[item.index].handle}
+                  </span>
+                </button>
+              );
+            }}
+          </For>
+        </div>
+      </div>
+    </>
   );
 }
 
